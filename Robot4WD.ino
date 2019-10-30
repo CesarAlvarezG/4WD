697a3eafe977a54405c89bf354fa76b455cd9bce
Cesar Alvarez <ing.cesaralvarezg@gmail.com>
2019-10-30 08:14:35 -0500

    Inclusión de los pines de ENABLE para el manejo de los motores

--- Robot4WD.ino
+++ Robot4WD.ino
@@ -1,37 +1,59 @@
 /*
-  Blink
+Programa para manejar un robot 4WD
+Modificado: 30/10/2019
+Ing. César Augusto Alvarez
 
-  Turns an LED on for one second, then off for one second, repeatedly.
 
-  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
-  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
-  the correct LED pin independent of which board is used.
-  If you want to know what pin the on-board LED is connected to on your Arduino
-  model, check the Technical Specs of your board at:
-  https://www.arduino.cc/en/Main/Products
-
-  modified 8 May 2014
-  by Scott Fitzgerald
-  modified 2 Sep 2016
-  by Arturo Guadalupi
-  modified 8 Sep 2016
-  by Colby Newman
-
-  This example code is in the public domain.
-
-  http://www.arduino.cc/en/Tutorial/Blink
 */
 
-// the setup function runs once when you press reset or power the board
+#define IN1Motor 2
+#define IN2Motor 4
+#define IN3Motor 6
+#define IN4Motor 7
+#define ENAMotor 3//Enable del motor Derecho
+#define ENBMotor 5//Enable del motor Izquierdo
+
+//Configuración del robot
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(IN1Motor, OUTPUT);
+  pinMode(IN2Motor, OUTPUT);
+  pinMode(IN3Motor, OUTPUT);
+  pinMode(IN4Motor, OUTPUT);
+  pinMode(ENAMotor, OUTPUT);
+  pinMode(ENBMotor, OUTPUT);
 }
 
-// the loop function runs over and over again forever
+//Ordenes para el robot
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
+  //Avanzar
+  digitalWrite(ENAMotor,HIGH);
+  digitalWrite(IN1Motor, HIGH);//Motor derecho
+  digitalWrite(IN2Motor, LOW);
+  
+  digitalWrite(ENBMotor,HIGH);
+  digitalWrite(IN3Motor, HIGH);//Motor Izquierdo
+  digitalWrite(IN4Motor, LOW);
+  delay(5000);
+           
+  //Retroceder    
+  digitalWrite(ENAMotor,HIGH);   
+  digitalWrite(IN1Motor, LOW);//Motor derecho
+  digitalWrite(IN2Motor, HIGH);
+
+  digitalWrite(ENBMotor,HIGH);
+  digitalWrite(IN3Motor, LOW);//Motor izquierdo
+  digitalWrite(IN4Motor, HIGH);
+  delay(5000);                
+
+  //Detener       
+  digitalWrite(ENAMotor,LOW);
+  digitalWrite(IN1Motor, LOW);//Motor derecho
+  digitalWrite(IN2Motor, LOW);
+
+  digitalWrite(ENAMotor,LOW);
+  digitalWrite(IN3Motor, LOW);//Motor izquierdo
+  digitalWrite(IN4Motor, LOW);
+  delay(5000);                
 }
