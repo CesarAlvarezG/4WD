d984558c72eebdd722fb0fc830359cc443ef56ce
Cesar Alvarez <ing.cesaralvarezg@gmail.com>
2019-11-01 11:13:49 -0500

    Implementación de una máquina de estados

--- Robot4WD.ino
+++ Robot4WD.ino
@@ -1,7 +1,9 @@
 /*
 Programa para manejar un robot 4WD
-Modificado: 30/10/2019
+Modificado: 1/11/2019
 Ing. César Augusto Alvarez
+
+Se incluye una maquina de estados
 */
 
 #include "MovRobot.h"  //Movimiento del robot
@@ -25,21 +27,46 @@ void setup() {
   Serial.begin(9600);
 }
 
+int estadoActual;
+
 //Ordenes para el robot
 void loop() {
-  
- avanzar();
- delay(2000);
- retroceder();
- delay(2000);
- detener();
- delay(2000);
- girarDerecha();
- delay(2000);
- girarIzquierda();
- delay(2000);
- detener();
- delay(2000);
- Serial.println(ostaculo());
- delay(200);     
+ int d;//Distancia del ostaculo
+ //Lógica de estado
+ d=ostaculo();
+ Serial.print(d);
+switch(estadoActual)
+{
+  case 0:
+          if(d>=30)
+            {
+             estadoActual=1; 
+            }
+          break;
+  case 1:
+          if(d<20)
+            {
+             estadoActual=0; 
+            }
+          break;
+}
+Serial.print("\t");
+Serial.println(estadoActual);
+ //Lógica de salida
+ switch(estadoActual)
+{
+  case 0:
+          detener();
+          break;
+  case 1:
+          avanzar();
+          break;
+  case 3: 
+          girarDerecha();
+          break;
+  case 4:
+          girarIzquierda();        
+          break;
+}
+// delay(200);
 }
